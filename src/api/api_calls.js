4bf89c3550c8a96c2978b7700e04e2b44a569221
Lynette <lynettecargo@Lynettes-Air.home>
2023-07-23 12:48:19 -0500

    mapped each villager data

--- src/api/api_calls.js
+++ src/api/api_calls.js
@@ -1,4 +1,6 @@
-const baseUrl = 'https://acnhapi.com/v1'
+// easy export of apis
+
+const baseUrl = 'https://acnhapi.com/v1a'
 
 export function getVillagers(){
     return fetch(`${baseUrl}/villagers`)
--- src/pages/Villagers.jsx
+++ src/pages/Villagers.jsx
@@ -6,20 +6,28 @@ import { getVillagers } from '../api/api_calls'
 
 function Villagers(props) {
   // state
-  const [villagers, setVillagers] = useState()
+  const [villagers, setVillagers] = useState([]) // data is an array
   // use api
   useEffect(()=>{
     // call api
     getVillagers()
     // grab data from api
-    .then(villagerData => console.log(villagerData))
-  })
+    // .then(villagerData => console.log(villagerData)) // check if fetched data successfully
+    // villagerData is an array
+    .then(villagerData => setVillagers(villagerData)) // villager data from api (save data to state)
+  }, [])
 
   return (
     <MainLayout>
       <div>
         <h1>*VILLAGERS PAGE*</h1>
-        <Villager />
+        {/* map each villager data to a component and pass state*/}
+        {villagers.map((villager) => {
+          return (
+            // villager.id = each villager has unique id from api
+            <Villager key={villager.id} villager={villager}/>
+          )
+        })}
       </div>
     </MainLayout>
   )
