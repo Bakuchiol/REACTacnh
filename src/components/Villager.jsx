301b119870a24ff68f6777f25a315baf6cb01c9c
Lynette <lynettecargo@Lynettes-Air.home>
2023-07-23 12:52:59 -0500

    naming convention problem from api

--- src/components/Villager.jsx
+++ src/components/Villager.jsx
@@ -4,6 +4,8 @@ function Villager(props) {
   return (
     <div>
       <h2>Villager Component</h2>
+      {/* problem pulling data (property name convention is - i.e. file-name) */}
+      <h3>{props.villager.name['name-USen']}</h3>
     </div>
   )
 }
